be098cea305259dd1df294b995a3e35bd1e7ee1f
mufeeda1996 <96809025+mufeeda1996@users.noreply.github.com>
2023-10-08 19:07:17 +0400

    features

--- src/features/Layout/Layout.js
+++ src/features/Layout/Layout.js
@@ -1,40 +1,22 @@
 import { Container } from "@mui/material";
 import React from "react";
 import AddressBar from "../../components/AddressBar";
-import Features from "../../components/Features";
 import MainNav from "../../components/MainNav";
 
 const Layout = ({ children }) => {
-  const features = [
-    {
-      title: "SAFETY",
-      image:
-        "https://alameentransportuae.com/static/media/safeIcon.b137857f.svg",
-      description: `Offering passangers peace of mind with meticulously trained and
-    experienced drivers,top-of-the-line vehicles equipped with advanced
-    safety features, and rigorous adherence to strict safety protocols.`,
-    },
-    {
-      title: "RELIABILITY",
-      image:
-        "https://alameentransportuae.com/static/media/reliableIcon.b32b4f53.svg",
-      description: `Ensuring punctuality,seamless coordination, and consistence 
-      highr-quality service,allowing clients to confidently rely on their 
-      chauffeur for all their transportation needs.`,
-    },
-    {
-      title: "AFFORDABLE",
-      image:
-        "https://alameentransportuae.com/static/media/afforIcon.690f2956.svg",
-      description: `With competitive pricing,offering value for money while
-       maintaining the highest standards of comfort,elegance,and personalised service.`,
-    },
-  ];
+  
+    const navItems = [
+      { label: 'HOME', url: "/" },
+      { label: 'ABOUT US', url: "/about" },
+      { label: 'COMPANY', url: "/contact" },
+      { label: 'CONTACT US', url: "/contact" },
+    ];
+  
   return (
     <Container maxWidth="100%" sx={{ padding: "0 !important" }}>
       <AddressBar />
       <MainNav
-        navMenu={["HOME", "ABOUT US", "COMPANY", "CONTACT US"]}
+         const navItems ={navItems}
         address={[
           " Abu Dhabi, UAE",
           "Dubai,UAE",
@@ -45,15 +27,6 @@ const Layout = ({ children }) => {
       />
 
       {children}
-      <Features
-        features={features}
-        backgroundColor={{ backgroundColor: "white.light" }}
-        boxShadow={{boxShadow:"0"}}
-        margin={{margin:"0"}}
-        imageWidth={{width:"50px"}}
-        imageBorderRadius={{borderRadius: "50px"}}
-        imageBackground={{backgroundColor:"brown"}}
-      />
     </Container>
   );
 };
--- src/features/Pages/HomePage.js
+++ src/features/Pages/HomePage.js
@@ -4,9 +4,36 @@ import Layout from "../Layout/Layout";
 import homeHero from "../../assets/home-hero.jpg";
 import { useDispatch } from "react-redux";
 import { openForm } from "../Slices/HomepageSlice";
+import Featuress from "../../components/Featuress";
+import { Container } from "@mui/material";
 
 const HomePage = () => {
   const dispatch = useDispatch();
+  const features = [
+    {
+      title: "SAFETY",
+      image:
+        "https://alameentransportuae.com/static/media/safeIcon.b137857f.svg",
+      description: `Offering passangers peace of mind with meticulously trained and
+    experienced drivers,top-of-the-line vehicles equipped with advanced
+    safety features, and rigorous adherence to strict safety protocols.`,
+    },
+    {
+      title: "RELIABILITY",
+      image:
+        "https://alameentransportuae.com/static/media/reliableIcon.b32b4f53.svg",
+      description: `Ensuring punctuality,seamless coordination, and consistence 
+      highr-quality service,allowing clients to confidently rely on their 
+      chauffeur for all their transportation needs.`,
+    },
+    {
+      title: "AFFORDABLE",
+      image:
+        "https://alameentransportuae.com/static/media/afforIcon.690f2956.svg",
+      description: `With competitive pricing,offering value for money while
+       maintaining the highest standards of comfort,elegance,and personalised service.`,
+    },
+  ];
   return (
     <Layout>
       <Hero
@@ -18,6 +45,11 @@ const HomePage = () => {
         btnLabel="Enquire now"
         btnFn={() => dispatch(openForm())}
       />
+     
+      <Container maxWidth="lg" sx={{margin: '3rem auto'}}>
+        <Featuress 
+         features={features}/>
+      </Container>
     </Layout>
   );
 };
